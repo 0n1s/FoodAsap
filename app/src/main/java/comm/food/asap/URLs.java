476d122fd423c5637f007a6efc17e8865c1a5a46
Momanyi <jjmomanyis@gmail.com>
2018-01-19 16:00:57 +0300

    initial release

---
+++ app/src/main/java/comm/food/asap/URLs.java
@@ -0,0 +1,13 @@
+package comm.food.asap;
+
+/**
+ * Created by sikinijjs on 10/1/17.
+ */
+
+public class URLs {
+
+    //http://code0.co.ke/dkutrest1/pages/index.php
+    public static String main_url="http://192.168.43.245/dkutrest1/android/";
+    //http://localhost/cladi/pages/images/
+    public static String img_url="http://192.168.43.245/dkutrest1/pages/images/";
+}
